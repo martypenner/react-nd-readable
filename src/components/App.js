4ce459267622e76781a412f4367baee9baae01a9
Marty Penner <penner.marty@gmail.com>
2017-09-07 15:20:38 -0400

    feat: Use material UI for initial test

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
+import FlatButton from 'material-ui/FlatButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React from 'react';
 
-const App = () => <div>Hello world</div>;
+const App = () => (
+  <MuiThemeProvider>
+    <div>
+      <FlatButton>Hello world</FlatButton>
+    </div>
+  </MuiThemeProvider>
+);
 
 export default App;
