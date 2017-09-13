fe49106e8ef57babe087da32c4a0659986039987
Marty Penner <penner.marty@gmail.com>
2017-09-13 19:59:11 -0400

    feat: Add an empty state for posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const EmptyPosts = () => (
+  <div className="mui--text-dark-secondary">No posts were found</div>
+);
+
 const Posts = ({ posts }) => (
   <div>
     <h2>Posts</h2>
@@ -10,6 +14,8 @@ const Posts = ({ posts }) => (
         <Link to={`/${post.category}/${post.id}`}>{post.title}</Link>
       </div>
     ))}
+
+    {posts.length === 0 && <EmptyPosts />}
   </div>
 );
 
