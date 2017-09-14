f10877794955ad2c09d8bde1d1ae6ed631ffbdb9
Marty Penner <penner.marty@gmail.com>
2017-09-13 21:50:46 -0400

    chore: Get redux devtools running

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { createStore } from 'redux';
 
-import reducers from './';
+import rootReducer from './';
 
-const store = createStore(reducers);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
